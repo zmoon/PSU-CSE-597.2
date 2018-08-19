1e6631104d50f910164b25a92e27ff62a73275d0
Zachary Moon <zlm1@psu.edu>
2018-08-19 15:20:51 -0400

    Implement hello world

---
+++ hw0/hi.f95
@@ -0,0 +1,12 @@
+!-------------------------------------------------------------------------------
+! CSE 597.2 hw0 - hello world program
+! - print the classic phrase to the command line
+! - due Mon 27-Aug-18
+! 
+! Zach Moon
+!===============================================================================
+
+program hi
+  write(*,*) "Hello, World!"
+end program hi
+
